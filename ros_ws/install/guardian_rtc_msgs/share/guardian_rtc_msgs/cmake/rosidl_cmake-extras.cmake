90ff91dd27aa236338c13e4a27f2ea5ce0e43b03
Eric Rosenfeld <ericalanrosenfeld@gmail.com>
2026-06-23 14:01:13 -0700

    Remove committed ROS 2 build output; gitignore install/build/log

--- ros_ws/install/guardian_rtc_msgs/share/guardian_rtc_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(guardian_rtc_msgs_IDL_FILES "msg/SystemStatus.idl;msg/SafetyStatus.idl;msg/ControlCommand.idl;msg/SystemConfig.idl;msg/StopEvent.idl")
-set(guardian_rtc_msgs_INTERFACE_FILES "msg/SystemStatus.msg;msg/SafetyStatus.msg;msg/ControlCommand.msg;msg/SystemConfig.msg;msg/StopEvent.msg")
